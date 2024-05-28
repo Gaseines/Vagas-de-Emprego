8dc903ec672be4cc6d05892a6b53250b13a0d471
Gaseines <gaseinesdzn@hotmail.com>
2024-05-28 19:24:57 -0300

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize')
 const sequelize = new 
 Sequelize('vagas_de_emprego', 'vagas_de_emprego_user', 'Kop74pr7D6EMT4NEb0pe9c4EsrFcuQpJ', {
     host: 'dpg-cpb54e5ds78s73epaq30-a',
-    dialect: 'psql'
+    dialect: 'postgres'
     
 })
 
