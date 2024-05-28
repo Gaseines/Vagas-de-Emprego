d5c2f62d6a397cd6b0fa4d6b51e695f4fd808a6b
Gaseines <gaseinesdzn@hotmail.com>
2024-05-28 19:49:29 -0300

    postgres

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,10 @@
 const Sequelize = require('sequelize')
 
 const sequelize = new 
-Sequelize('vagas_de_emprego', 'vagas_de_emprego_user', 'Kop74pr7D6EMT4NEb0pe9c4EsrFcuQpJ', {
-    host: 'dpg-cpb54e5ds78s73epaq30-a',
-    dialect: 'postgres'
+Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false
     
 })
 
