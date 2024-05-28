6343f2b47cfe8c26b7a69bcff29b04e58c7d8978
Gaseines <gaseinesdzn@hotmail.com>
2024-05-27 23:08:11 -0300

    terminando projeto

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
-const express    = require('express')
-const app        = express()
-const db         = require('./db/connection')
+// Importa o módulo Express para criar o servidor
+const express = require('express')
+
+// Importa o módulo express-handlebars para gerenciar templates
+const exphbs = require('express-handlebars')
+
+// Importa o módulo path para trabalhar com caminhos de diretórios
+const path = require('path')
+
+// Cria uma instância do aplicativo Express
+const app = express()
+
+// Importa o módulo de conexão com o banco de dados
+const db = require('./db/connection')
+
+// Importa o módulo body-parser para processar dados do corpo da requisição
 const bodyParser = require('body-parser')
 
+const Job = require('./models/Job')
 
+const Sequelize = require('sequelize')
+
+const Op = Sequelize.Op
+
+// Define a porta em que o servidor vai rodar
 const PORT = 3000
 
+// Inicia o servidor na porta definida e exibe uma mensagem no console
 app.listen(PORT, function(){
     console.log(`Está rodando na porta ${PORT}`)
 })
 
-// body parser
+// Configura o body-parser para processar dados do corpo da requisição com URL encoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(bodyParser.urlencoded({  extended: false  }))
+// Configura o handlebars como motor de visualização e define o diretório de views
+app.set('views', path.join(__dirname, 'views'))
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
-//db connection
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
+// Conecta ao banco de dados e exibe uma mensagem no console conforme o resultado
 db
     .authenticate()
     .then(() => {
@@ -25,12 +51,37 @@ db
         console.log("Ocorreu um erro ao conectar o banco de dados")
     })
 
-
-//Routes
+// Define uma rota raiz que envia uma resposta simples
 app.get('/', (req, res) => {
-    res.send("Está funcionando!")
+
+    let search = req.query.job
+    let query = '%'+search+'%'
+
+    if(!search){
+        Job.findAll({order: [
+            ['createdAt', 'DESC']
+        ]})
+        .then(jobs => {
+            res.render('index', {
+                jobs
+            })
+        })
+        .catch(err => console.log(err))
+    } else{
+        Job.findAll({
+           where: {title: {[Op.like]: query}}, 
+            order: [
+            ['createdAt', 'DESC']
+        ]})
+        .then(jobs => {
+            res.render('index', {
+                jobs, search
+            })
+        })
+    }
+
+    
 })
 
-//jobs routes
-
+// Define as rotas para /jobs, utilizando um roteador externo
 app.use('/jobs', require('./routes/jobs'))
